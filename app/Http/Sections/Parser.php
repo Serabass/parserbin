bf8944e07dbb76ec9036060eae17356b1bd5040c
Serabass <is_everything@mail.ru>
2018-04-08 06:17:56 +0600

    SleepingOwl!

---
+++ app/Http/Sections/Parser.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Parserbin\Http\Sections;
+
+use Illuminate\Support\Facades\URL;
+use SleepingOwl\Admin\Contracts\Display\DisplayInterface;
+use SleepingOwl\Admin\Contracts\Form\FormInterface;
+use SleepingOwl\Admin\Section;
+
+use AdminColumn;
+use AdminColumnEditable;
+use AdminColumnFilter;
+use AdminDisplay;
+use AdminFormElement;
+
+/**
+ * Class Parser
+ *
+ * @property \Parserbin\Models\Parser $model
+ *
+ * @see http://sleepingowladmin.ru/docs/model_configuration_section
+ */
+class Parser extends Section
+{
+    /**
+     * @see http://sleepingowladmin.ru/docs/model_configuration#ограничение-прав-доступа
+     *
+     * @var bool
+     */
+    protected $checkAccess = false;
+
+    /**
+     * @var string
+     */
+    protected $title;
+
+    /**
+     * @var string
+     */
+    protected $alias;
+
+    /**
+     * @return DisplayInterface
+     */
+    public function onDisplay()
+    {
+        $display = AdminDisplay::datatablesAsync();
+        $display->setColumns([
+            AdminColumnEditable::text('title')->setLabel('Название'),
+            AdminColumnEditable::text('hash')->setLabel('Хеш'),
+            AdminColumn::datetime('created_at')
+                ->setLabel('Создан')
+                ->setFormat('d.m.Y h:i:s'),
+
+            AdminColumn::datetime('updated_at')
+                ->setLabel('Обновлён')
+                ->setFormat('d.m.Y h:i:s'),
+        ]);
+
+        $display->setColumnFilters([
+            AdminColumnFilter::text()
+                ->setColumnName('title')
+                ->setPlaceholder('Название')
+                ->setOperator('contains'),
+
+            AdminColumnFilter::text()
+                ->setColumnName('hash')
+                ->setPlaceholder('Хеш')
+                ->setOperator('contains'),
+
+        ]);
+
+        $display->getColumnFilters()
+            ->setPlacement('panel.heading');
+
+        return $display;
+
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return FormInterface
+     */
+    public function onEdit($id)
+    {
+        $form = \AdminForm::form();
+        $form->setElements([
+            AdminFormElement::text('title', 'Название'),
+            $id ? AdminFormElement::textaddon('hash', 'Хеш')
+                ->required(true)
+                ->setAddon(URL::to('/p/') . '/') : null,
+
+            AdminFormElement::selectajax('userId', 'Пользователь')
+                ->setModelForOptions(\Parserbin\User::class),
+
+            AdminFormElement::textarea('input', 'Входящие данные')
+        ]);
+        return $form;
+
+    }
+
+    /**
+     * @return FormInterface
+     */
+    public function onCreate()
+    {
+        return $this->onEdit(null);
+    }
+
+    /**
+     * @return void
+     */
+    public function onDelete($id)
+    {
+        // remove if unused
+    }
+
+    /**
+     * @return void
+     */
+    public function onRestore($id)
+    {
+        // remove if unused
+    }
+}
---
+++ app/Providers/AdminSectionsServiceProvider.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Parserbin\Providers;
+
+use AdminNavigation;
+use KodiCMS\Assets\Facades\PackageManager;
+use Parserbin\Models\Parser;
+use SleepingOwl\Admin\Contracts\Widgets\WidgetsRegistryInterface;
+use SleepingOwl\Admin\Navigation\Page;
+use SleepingOwl\Admin\Providers\AdminSectionsServiceProvider as ServiceProvider;
+
+class AdminSectionsServiceProvider extends ServiceProvider
+{
+
+    /**
+     * @var array
+     */
+    protected $sections = [
+        \Parserbin\User::class => 'Parserbin\Http\Sections\Users',
+        \Parserbin\Models\Parser::class => 'Parserbin\Http\Sections\Parser',
+        \Parserbin\Models\Script::class => 'Parserbin\Http\Sections\Script',
+        \Parserbin\Models\Language::class => 'Parserbin\Http\Sections\Language',
+    ];
+
+    /**
+     * Register sections.
+     *
+     * @return void
+     */
+    public function boot(\SleepingOwl\Admin\Admin $admin)
+    {
+    	//
+
+        parent::boot($admin);
+
+        $this->registerNRoutes();
+        $this->registerNavigation();
+        $this->registerMediaPackages();
+        $this->app->call([$this, 'registerViews']);
+
+    }
+
+    private function registerNavigation()
+    {
+        $pages = [
+            [\Parserbin\Models\Parser::class, 300, 'Парсеры'],
+            [\Parserbin\Models\Language::class, 300, 'Языки'],
+            [\Parserbin\Models\Script::class, 300, 'Скрипты'],
+            [\Parserbin\User::class, 300, 'Пользователи'],
+        ];
+
+        foreach ($pages as $page) {
+            list($class, $priority, $title) = $page;
+            $page = new Page($class);
+
+            $page
+                ->setPriority($priority)
+                ->setTitle($title);
+
+            AdminNavigation::addPage($page);
+        }
+    }
+
+    private function registerNRoutes()
+    {
+        /**
+         * @var \Illuminate\Routing\Router $router
+         */
+        $router = $this->app['router'];
+        $router->group([
+            'prefix' => config('sleeping_owl.url_prefix'),
+            'middleware' => config('sleeping_owl.middleware')], function ($router) {
+            $router->get('', ['as' => 'admin.dashboard', function () {
+                $content = 'Define your dashboard here.';
+                return AdminSection::view($content, 'Dashboard');
+            }]);
+        });
+    }
+
+    private function registerMediaPackages()
+    {
+        PackageManager::add('front.controllers')
+            ->js(null, asset('js/controllers.js'));
+    }
+
+    /**
+     * @param WidgetsRegistryInterface $widgetsRegistry
+     */
+    public function registerViews(WidgetsRegistryInterface $widgetsRegistry)
+    {
+        // foreach ($this->widgets as $widget) {
+        //     $widgetsRegistry->registerWidget($widget);
+        // }
+    }
+
+}
