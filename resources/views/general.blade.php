f2b111d5f82d21e82cc3dc4a197c8dba529486a1
Serabass <is_everything@mail.ru>
2018-04-08 08:26:33 +0600

    upd

--- resources/views/general.blade.php
+++ resources/views/general.blade.php
@@ -18,7 +18,7 @@
     <link href="/css/editor.css" rel="stylesheet" type="text/css">
     <link href="/css/login.css" rel="stylesheet" type="text/css">
 
-    <link rel="shortcut icon" href="/images/favicon.png" />
+    <link rel="shortcut icon" href="/images/favicon.png"/>
 
     <script src="/js/editor.js"></script>
 </head>
@@ -28,17 +28,24 @@
          src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub">
 </a>
 <div class="toolbar">
-    <a href="/" class="logo-link">
-        <img src="/images/logo.png" alt="{parserbin}" class="logo" />
-    </a>
-    <button id="new">New</button>
-    <button id="save">Save</button>
-    <button id="evaluate">Eval</button>
-    <button id="toggle-code">Toggle Code</button>
-    <label for="auto-update">
-        <input type="checkbox" id="auto-update">
-        Auto update
-    </label>
+    <div class="buttons">
+        <a href="/" class="logo-link">
+            <img src="/images/logo.png" alt="{parserbin}" class="logo"/>
+        </a>
+        <button id="new">New</button>
+        <button id="save">Save</button>
+        <button id="evaluate">Eval</button>
+        <button id="toggle-code">Toggle Code</button>
+        <label for="auto-update">
+            <input type="checkbox" id="auto-update">
+            Auto update
+        </label>
+    </div>
+    <div class="powered">
+        powered by <a href="http://serabass.net" target="_blank">
+            Serabass
+        </a>
+    </div>
 
     @if (Route::has('login'))
         <div class="top-right links">
