23677c017c7d65e69570aa061f5014c81cb6b9bc
Serabass <is_everything@mail.ru>
2018-04-11 19:11:23 +0600

    Create social-icons.blade.php

---
+++ resources/views/auth/social-icons.blade.php
@@ -0,0 +1,15 @@
+<ul class="social-icons">
+  <li>
+    <a href="#">
+      <i class="fa fa-github"></i>
+    </a>
+    <a href="#">
+      <i class="fa fa-facebook-f"></i>
+    </a>
+    <a href="#">
+      <i class="fa fa-vk"></i>
+    </a>
+    <a href="#">
+      <i class="fa fa-google"></i>
+    </a>
+</ul>
