bcaeda21e7064b6bf5cc2cde3777e0e326d210ea
Serabass <is_everything@mail.ru>
2018-04-12 16:51:20 +0600

    hidden non working social icons

--- resources/views/auth/social-icons.blade.php
+++ resources/views/auth/social-icons.blade.php
@@ -1,27 +1,27 @@
 <ul class="social-icons">
-  <li>
-    <a href="{{ route('login-social.github') }}" title="Github">
-      <i class="fab fa-github"></i>
-    </a>
-  </li>
-  <li>
-    <a href="#" title="Facebook">
-      <i class="fab fa-facebook-f"></i>
-    </a>
-  </li>
-  <li>
-    <a href="#" title="VKontakte">
-      <i class="fab fa-vk"></i>
-    </a>
-  </li>
-  <li>
-    <a href="#" title="Google+">
-      <i class="fab fa-google-plus-g"></i>
-    </a>
-  </li>
-  <li>
-    <a href="#" title="Twitter">
-      <i class="fab fa-twitter"></i>
-    </a>
-  </li>
+    <li>
+        <a href="{{ route('login-social.github') }}" title="Github">
+            <i class="fab fa-github"></i>
+        </a>
+    </li>
+    <li hidden>
+        <a href="#" title="Facebook">
+            <i class="fab fa-facebook-f"></i>
+        </a>
+    </li>
+    <li hidden>
+        <a href="#" title="VKontakte">
+            <i class="fab fa-vk"></i>
+        </a>
+    </li>
+    <li hidden>
+        <a href="#" title="Google+">
+            <i class="fab fa-google-plus-g"></i>
+        </a>
+    </li>
+    <li hidden>
+        <a href="#" title="Twitter">
+            <i class="fab fa-twitter"></i>
+        </a>
+    </li>
 </ul>
