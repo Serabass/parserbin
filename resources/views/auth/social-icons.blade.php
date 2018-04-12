f1e58718e541efb605d9fb7a91c17206789ce164
Serabass <is_everything@mail.ru>
2018-04-12 16:53:02 +0600

    hidden non working social icons

--- resources/views/auth/social-icons.blade.php
+++ resources/views/auth/social-icons.blade.php
@@ -4,22 +4,22 @@
             <i class="fab fa-github"></i>
         </a>
     </li>
-    <li hidden>
+    <li class="hidden">
         <a href="#" title="Facebook">
             <i class="fab fa-facebook-f"></i>
         </a>
     </li>
-    <li hidden>
+    <li class="hidden">
         <a href="#" title="VKontakte">
             <i class="fab fa-vk"></i>
         </a>
     </li>
-    <li hidden>
+    <li class="hidden">
         <a href="#" title="Google+">
             <i class="fab fa-google-plus-g"></i>
         </a>
     </li>
-    <li hidden>
+    <li class="hidden">
         <a href="#" title="Twitter">
             <i class="fab fa-twitter"></i>
         </a>
